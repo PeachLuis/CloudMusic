fd611e8c957066dcc6488f91b2aa831eee20b7ad
PeachLuis <1527725645@qq.com>
2018-12-07 18:10:20 +0800

    CloudMusic Imitated
    A homepage imitating CloudMusic

---
+++ app/src/main/java/com/example/cloudmusic/PhoneLogup.java
@@ -0,0 +1,13 @@
+package com.example.cloudmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneLogup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_logup2);
+    }
+}
